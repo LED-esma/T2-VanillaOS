5d05faf3d3d71ec0f0e9d996bac1fa28ac048516
ramon ledesma <98295103+LED-esma@users.noreply.github.com>
2023-08-01 16:56:08 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -81,8 +81,8 @@ build () {
           #T2 customization
         mkdir -p /etc/apt/sources.list.d
 
-        curl -s --compressed "https://adityagarg8.github.io/t2-ubuntu-repo/KEY.gpg" | gpg --dearmor | tee /etc/apt/trusted.gpg.d/t2-ubuntu-repo.gpg >/dev/null
-        curl -s --compressed -o /etc/apt/sources.list.d/t2.list "https://adityagarg8.github.io/t2-ubuntu-repo/t2.list"
+        curl -s --compressed "https://adityagarg8.github.io/t2-ubuntu-repo/KEY.gpg" | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/t2-ubuntu-repo.gpg >/dev/null
+        sudo curl -s --compressed -o /etc/apt/sources.list.d/t2.list "https://adityagarg8.github.io/t2-ubuntu-repo/t2.list"
         apt-get update 
 
         curl -L https://github.com/t2linux/T2-Ubuntu-Kernel/releases/download/v6.4.7-1/linux-headers-6.4.7-t2_6.4.7-1_amd64.deb > /tmp/headers.deb
